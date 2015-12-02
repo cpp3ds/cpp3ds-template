3f261157ebeeddfd2a597c5473a69a064edb8f81
Thomas Edvalson <machin3@gmail.com>
2015-12-02 16:36:27 -0500

    Add namespacing and update RSF for latest Makerom

--- src/main.cpp
+++ src/main.cpp
@@ -2,16 +2,15 @@
 #include <cpp3ds/System.hpp>
 #include <cpp3ds/Window.hpp>
 
-using namespace cpp3ds;
 
 // Minimal derived Game class necessary for cpp3ds games
-class MyGame: public Game {
+class MyGame: public cpp3ds::Game {
 private:
-	Text text;
+	cpp3ds::Text text;
 public:
 	MyGame() {
 		// Constructor for setting up initial game objects
-		text.setColor(Color::Green);
+		text.setColor(cpp3ds::Color::Green);
 		text.setCharacterSize(40);
 		text.setString(_("Hello World"));
 	}
@@ -25,11 +24,11 @@ public:
 		text.move(10.f * delta, 0); // Move 10 pixels per second to the right
 	}
 
-	void processEvent(Event& event) {
+	void processEvent(cpp3ds::Event& event) {
 		// Check for input Events
 		switch (event.type) {
-			case Event::KeyPressed:
-				if (event.key.code == Keyboard::Select)
+			case cpp3ds::Event::KeyPressed:
+				if (event.key.code == cpp3ds::Keyboard::Select)
 					exit(); // Exit game when Select is pressed
 				break;
 			default:
@@ -37,19 +36,20 @@ public:
 		}
 	}
 
-	void renderTopScreen(Window& window) {
+	void renderTopScreen(cpp3ds::Window& window) {
 		window.clear();
 		window.draw(text);
 	}
 
-	void renderBottomScreen(Window& window) {
+	void renderBottomScreen(cpp3ds::Window& window) {
 		window.clear();
 	}
 };
 
+
 int main(int argc, char** argv) {
 	MyGame game;
-	game.console(BottomScreen); // Console for reading stdout
+	game.console(cpp3ds::BottomScreen); // Console for reading stdout
 	game.run();
 	return 0;
 }
